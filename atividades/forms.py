2c1e047c7e8c590692a5cfcee85468079bf84406
Manilha7 <a58604@ualg.pt>
2020-03-20 21:51:56 +0000

    Up

--- atividades/forms.py
+++ atividades/forms.py
@@ -1,8 +1,18 @@
-from django import forms
-from .models import Atividade
+from django import forms  
+from .models import Atividade, Sessao, Horario  
 
+class AtividadeForm(forms.ModelForm):  
+    class Meta:  
+        model = Atividade  
+        exclude = ['coordenadorutilizadorid', 'professoruniversitarioutilizadorid','datasubmissao', 'dataalteracao','estado','id']
 
-class AtividadeForm(forms.ModelForm):
-    class Meta:
-        model = Atividade
-        fields = "__all__"
+class SessaoForm(forms.ModelForm):  
+    class Meta:  
+        model = Sessao  
+        exclude = ["espacoid", "horarioid","vagas","ninscritos"]
+
+class HorarioForm(forms.ModelForm):  
+    class Meta:  
+        model = Horario
+        exclude = ["id", "participantesmaximo"]  
+
