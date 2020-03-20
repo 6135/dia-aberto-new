85a488c76f327ef5bb1c16e28cfcc7165a2d6925
6135 <guibscosta@hotmail.com>
2020-03-20 21:05:17 +0000

    cxvf

--- atividades/models.py
+++ atividades/models.py
@@ -6,8 +6,8 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from utilizadores.models import Utilizador
-from coordenadores.models import Coordenador
+from utilizadores.models import *
+from coordenadores.models import *
 from configuracao.models import *
 from inscricoes.models import *
 from notificacoes.models import *
