e56afb3db496aaf53b364fb3f39f4ce96e38ce07
6135 <guibscosta@hotmail.com>
2020-03-22 20:19:04 +0000

    Added Settings page with a semi-working DateTimePicker

---
+++ configuracao/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm, DateTimeInput, DateInput
+from .models import *
+
+class DateTimeWidget(DateTimeInput):
+    class Media:
+        js = ('static/configuracao/js/jquery-ui-timepicker-addon.js',)
+    def __init__(self, attrs=None, format=None):
+        if attrs is not None:
+            self.attrs = attrs.copy()
+        else:
+            self.attrs = {'class': 'input datetimepicker'}
+        if format is not None:
+            self.format = format.copy()
+        else: 
+            self.format = '%Y-%m-%d %H:%M'
+
+class diaAbertoSettingsForm(ModelForm):  
+    class Meta:
+        model = Diaaberto
+        exclude = ['administradorutilizadorid', 'id']
+        widgets = {
+            'datadiaabertoinicio': DateTimeWidget(),
+            'datadiaabertofim': DateTimeWidget(),
+            'datapropostasatividadesincio': DateTimeWidget(),
+            'dataporpostaatividadesfim': DateTimeWidget(),
+            'datainscricaoatividadesinicio': DateTimeWidget(),
+            'datainscricaoatividadesfim': DateTimeWidget(),
+        }
+    
