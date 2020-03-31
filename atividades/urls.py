edb947ca01b28c1ebcc5d1ac6169d0202567e762
6135 <guibscosta@hotmail.com>
2020-03-31 17:10:26 +0100

    Update urls.py

--- atividades/urls.py
+++ atividades/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path("minhasatividades",views.minhasatividades,name="minhasAtividades"),
-    path("proporatividade",views.proporatividade,name="proporAtividade"),
     path('alteraratividade/<int:id>',views.alterarAtividade,name='alterarAtividade'),
-    path('inserirsessao/<id>',views.novasessao,name='inserirSessao'),
     path('alteraratividade/<id>',views.alterarAtividade,name='alterarAtividade'),
     path('alterarsessao/<id>',views.alterarSessao,name='alterarSessao'),
     path('eliminaratividade/<id>',views.eliminarAtividade,name='eliminarAtividade'),
