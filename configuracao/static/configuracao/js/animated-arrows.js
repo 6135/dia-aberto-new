1d9f3dd6dda99ef7fbcbf0eddc1de324053aafee
6135 <guibscosta@hotmail.com>
2020-04-13 01:40:37 +0100

    IM AM STUPID

--- configuracao/static/configuracao/js/animated-arrows.js
+++ configuracao/static/configuracao/js/animated-arrows.js
@@ -9,9 +9,9 @@ $('.pre-expandable').on('click', function() {
 $('.icon-expandable').on('click', function() {
     $(this).toggleClass('is-expanded');
 });
-
+/*
 $('.icon-expandable-180').on('click', function() {
     console.log("arrow clicked");
     $(this).toggleClass('is-expanded');
-});
+});*/
 
