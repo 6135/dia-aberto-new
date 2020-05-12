2f0226a6fdae92078c04c2d79983c65aaadd6115
André Barrocas <andrebarrocas99@hotmail.com>
2020-05-12 23:47:38 +0100

    integração das componentes

---
+++ colaboradores/templatetags/informacao.py
@@ -0,0 +1,135 @@
+from django import template
+from coordenadores.models import Tarefa,TarefaAcompanhar, TarefaAuxiliar,TarefaOutra
+from datetime import date, timedelta
+import datetime
+
+register = template.Library()
+
+@register.filter(name='get_due_date_string')
+def get_due_date_string(value,id):
+    temp= Tarefa.objects.get(id=id)
+    if temp.tipo=="tarefaAuxiliar":
+        tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+        value = tarefa.sessaoid.dia
+        passou = tarefa.sessaoid.horarioid.inicio.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S')
+    elif temp.tipo == "tarefaAcompanhar":
+        tarefa = TarefaAcompanhar.objects.get(tarefaid=id)  
+        value = tarefa.dia 
+        passou = tarefa.horario.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S') 
+    else:
+        tarefa = TarefaOutra.objects.get(tarefaid=id)  
+        value = tarefa.dia
+        passou = tarefa.horario.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S')
+    delta = value - date.today()
+    if temp.estado == "Cancelada":
+        return "Esta tarefa foi cancelada"
+    if delta.days == 0 and temp.estado != "Concluida":
+        return "Esta tarefa é hoje!"
+    elif  delta.days == 0 and temp.estado == "Concluida":
+        return "Esta tarefa foi hoje!"
+    elif delta.days < 1:
+        return "Esta tarefa foi há %s %s atrás!" % (abs(delta.days),
+            ("dia" if abs(delta.days) == 1 else "dias"))
+    elif delta.days == 1:
+        return "Esta tarefa é amanhã"
+    elif delta.days > 1:
+        return "Esta tarefa é daqui a %s dias" % delta.days
+
+
+@register.filter(name='tarefa_utrapassada')
+def tarefa_utrapassada(value,id):
+    temp= Tarefa.objects.get(id=id)
+    if temp.tipo=="tarefaAuxiliar":
+        tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+        value = tarefa.sessaoid.dia
+        passou = tarefa.sessaoid.horarioid.inicio.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S')
+    elif temp.tipo == "tarefaAcompanhar":
+        tarefa = TarefaAcompanhar.objects.get(tarefaid=id)  
+        value = tarefa.dia 
+        passou = tarefa.horario.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S') 
+    else:
+        tarefa = TarefaOutra.objects.get(tarefaid=id)  
+        value = tarefa.dia
+        passou = tarefa.horario.strftime('%H:%M:%S') < datetime.datetime.now().strftime('%H:%M:%S')
+    delta = value - date.today()
+
+    if delta.days == 0 and passou == True:
+        return True
+    elif delta.days > 0:
+        return True
+    elif delta.days == 0 and passou == False:
+        return False    
+    else :
+        return None    
+
+
+@register.filter(name='get_tarefa_auxiliar_prof') 
+def get_tarefa_auxiliar_prof(tarefa,id):
+    tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.sessaoid.atividadeid.professoruniversitarioutilizadorid.first_name+" "+tarefa.sessaoid.atividadeid.professoruniversitarioutilizadorid.last_name
+    else:
+        pass
+
+@register.filter(name='get_tarefa_auxiliar_campus') 
+def get_tarefa_auxiliar_campus(tarefa,id):
+    tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+    if tarefa!=None:    
+        return tarefa.sessaoid.atividadeid.espacoid.edificio.campus.nome
+    else:
+        pass
+
+@register.filter(name='get_tarefa_auxiliar_sala') 
+def get_tarefa_auxiliar_sala(tarefa,id):
+    tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.sessaoid.atividadeid.espacoid.edificio.campus.nome    
+    else:
+        pass
+
+@register.filter(name='get_tarefa_auxiliar_edificio') 
+def get_tarefa_auxiliar_edificio(tarefa,id):
+    tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.sessaoid.atividadeid.espacoid.edificio.nome
+    else:
+        pass
+
+@register.filter(name='get_tarefa_auxiliar_espaco') 
+def get_tarefa_auxiliar_espaco(tarefa,id):
+    tarefa = TarefaAuxiliar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.sessaoid.atividadeid.espacoid.nome
+    else:
+        pass
+###
+
+
+@register.filter(name='get_tarefa_acompanhar_origem') 
+def get_tarefa_acompanhar_origem(tarefa,id):
+    tarefa = TarefaAcompanhar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.origem
+    else:
+        pass
+@register.filter(name='get_tarefa_acompanhar_destino') 
+def get_tarefa_acompanhar_destino(tarefa,id):
+    tarefa = TarefaAcompanhar.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.destino
+    else:
+        pass 
+
+###
+
+
+@register.filter(name='get_tarefa_outra_descricao') 
+def get_tarefa_outra_descricao(tarefa,id):
+    tarefa = TarefaOutra.objects.get(tarefaid=id)
+    if tarefa!=None:
+        return tarefa.descricao
+    else:
+        pass    
+
+
+        
---
+++ static/js/js_custom.js
@@ -0,0 +1,120 @@
+
+
+var alert = new eliminar();
+function eliminar() {
+    this.render=function(txt,elementId){
+        var msg = document.getElementById("msg")
+        document.getElementById('text-00').innerHTML = txt;
+        document.getElementById('dialog-confirm').setAttribute('data-target',elementId)
+        msg.classList.add("is-active");
+    }
+    this.confirmar=function(){
+        var elementId = document.getElementById('dialog-confirm').getAttribute('data-target');
+        var href = document.getElementById(elementId).value;
+        window.location.href = href;
+    }
+    this.cancelar=function(){
+        var msg = document.getElementById("msg")
+        msg.classList.remove("is-active");
+    }   
+}
+
+var alert = new rejeitar();
+function rejeitar() {
+    this.render=function(txt,elementId){
+        var msg = document.getElementById("msg")
+        document.getElementById('text-00').innerHTML = txt;
+        document.getElementById('dialog-confirm').setAttribute('data-target',elementId)
+        msg.classList.add("is-active");
+    }
+    this.confirmar=function(){
+        var elementId = document.getElementById('dialog-confirm').getAttribute('data-target');
+        var href = document.getElementById(elementId).value;
+        window.location.href = href;
+    }
+    this.cancelar=function(){
+        var msg = document.getElementById("msg")
+        msg.classList.remove("is-active");
+    }   
+}
+
+
+
+function resetForm(){
+    oFormObject = document.forms['filter'];
+    elements = oFormObject.elements;
+    for (var i = 0, element; element = elements[i++];)
+        if(element.type != 'hidden')
+            element.value='';
+}
+
+function displayDetails(idForDetails){
+    var objectDetails = document.getElementById("Details"+idForDetails);
+    if(objectDetails.style.display == 'none') {
+        objectDetails.style.display = "contents";
+    } else objectDetails.style.display = "none";
+
+    var x = document.getElementById("mais_informacao"+idForDetails);
+    if (x.className === "mdi is-fullwidth mdi-chevron-right mdi-24px") {
+      x.className = "mdi is-fullwidth mdi-chevron-down mdi-24px";
+    } else {
+      x.className = "mdi is-fullwidth mdi-chevron-right mdi-24px";
+    }
+}
+
+function sortTable(n, isDate) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("table01");
+  switching = true;
+  dir = "asc";
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 3); i+=2) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 2].getElementsByTagName("TD")[n];
+        
+        if(isDate != true) {
+            xInner=x.innerHTML.toLowerCase();
+            yInner=y.innerHTML.toLowerCase();
+        } else {
+            xInner=new Date(x.getAttribute('date-value'));
+            yInner=new Date(y.getAttribute('date-value'));
+        } 
+        
+        if (dir == "asc") {
+            if (xInner > yInner) {
+                shouldSwitch = true;
+                break;
+            }
+        } else if (dir == "desc") {
+            if (xInner < yInner) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+    }
+    if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 2], rows[i]);
+        rows[i].parentNode.insertBefore(rows[i + 3], rows[i+1]);
+        switching = true;
+        switchcount ++;
+    } else {
+        if (switchcount == 0 && dir == "asc") {
+            dir = "desc";
+            switching = true;
+      }
+    }
+  }
+
+
+  var x = document.getElementById("ordenar");
+  if (x.className === "mdi mdi-menu-up") {
+    x.className = "mdi mdi-menu-down";
+  } else {
+    x.className = "mdi mdi-menu-up";
+  }
+}
+
+
