1790f6bb1f826243aad8300dbda6354040809ad9
André Barrocas <andrebarrocas99@hotmail.com>
2020-05-24 16:37:18 +0100

    urls notifcacoes app notificacoes

--- notificacoes/urls.py
+++ notificacoes/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('enviar', views.EnviarNotificacao, name="enviar-notificacao"),
     path('<int:pk>/', views.DetalhesNotificacao,
          name="detalhes-notificacao"),
-    path('enviarnotificacao', views.enviar_notificacao_automatica,
-         name='enviar-notificacao-automatica'),
+#     path('enviarnotificacao', views.enviar_notificacao_automatica,
+#          name='enviar-notificacao-automatica'),
+#     path('detalhesnotificacao/<int:id>', views.detalhes_notificacao_automatica,
+#          name='detalhes-notificacao-automatica'),
+         
 ]
