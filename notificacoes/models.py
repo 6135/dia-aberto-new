131fb5dffc010e913d8b15634c0b0c9e05d3450e
tiagonuneslx <tiago.nunes.lx@gmail.com>
2020-05-24 17:32:07 +0100

    MensagemRecebida e MensagemEnviada

--- notificacoes/models.py
+++ notificacoes/models.py
@@ -6,45 +6,27 @@ from notifications.base.models import AbstractNotification
 
 class Notificacao(AbstractNotification):
     titulo = models.CharField(max_length=255)
-    descricao = models.CharField(max_length=255) 
-    tipo = models.CharField(max_length=255)     
+    descricao = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
     class Meta(AbstractNotification.Meta):
         abstract = False
         db_table = 'Notificacao'
 
 
 
-class EmissorMensagem(models.Model):
+class MensagemRecebida(models.Model):
     mensagem = models.ForeignKey(
         Notificacao, models.CASCADE)
-    emissor = models.ForeignKey(
-        'utilizadores.Utilizador', models.CASCADE)
-
+        
     class Meta:
-        db_table = 'EmissorMensagem'
+        db_table = 'MensagemRecebida'
 
 
 
-class RecetorMensagem(models.Model):
+class MensagemEnviada(models.Model):
     mensagem = models.ForeignKey(
         Notificacao, models.CASCADE)
-    recetor = models.ForeignKey(
-        'utilizadores.Utilizador', models.CASCADE)
 
     class Meta:
-        db_table = 'RecetorMensagem'
+        db_table = 'MensagemEnviada'
 
-
-
-
-
-class NotificacaoRecebida(models.Model):
-    notificacao = models.ForeignKey(
-        Notificacao, models.CASCADE)
-    emissor = models.ForeignKey(
-        'utilizadores.Utilizador', models.CASCADE)
-    recetor = models.ForeignKey(
-        'utilizadores.Utilizador', models.CASCADE)
-
-    class Meta:
-        db_table = 'NotificacaoRecebida'
