46bc1da56d112650e91fad1cce336b38d4bf94a2
tiagonuneslx <tiago.nunes.lx@gmail.com>
2020-06-09 17:40:48 +0100

    Melhorias no algoritmo de deteção de conflitos nas inscrições de sessões

--- inscricoes/forms.py
+++ inscricoes/forms.py
@@ -55,19 +55,31 @@ def verificar_vagas(sessoes, nalunos):
     """
     inscritos_horarios = []
     for sessao in sessoes:
-        nalunos_horario = nalunos
-        sessao_obj = Sessao.objects.get(pk=sessao)
-        for inscritos_horario in inscritos_horarios:
-            if horarios_intersetam(sessao_obj.horarioid.inicio, sessao_obj.horarioid.fim, inscritos_horario['inicio'], inscritos_horario['fim']):
-                nalunos_horario -= inscritos_horario['ninscritos']
-        if sessoes[sessao] > nalunos_horario:
-            raise forms.ValidationError(
-                _(f"O número de inscritos na sessão da atividade \"{sessao_obj.atividadeid.nome}\", das {sessao_obj.horarioid.inicio.strftime('%H:%M')} às {sessao_obj.horarioid.fim.strftime('%H:%M')} ({sessoes[sessao]}) excede o nº de alunos disponíveis nesse horário ({nalunos_horario})"))
+        horario = Sessao.objects.get(pk=sessao).horarioid
         inscritos_horarios.append({
-            'inicio': sessao_obj.horarioid.inicio,
-            'fim': sessao_obj.horarioid.fim,
+            'sessao': sessao,
+            'inicio': horario.inicio,
+            'fim': horario.fim,
             'ninscritos': sessoes[sessao],
         })
+    for sessao in sessoes:
+        nalunos_horario = nalunos
+        sessao_obj = Sessao.objects.get(pk=sessao)
+        if sessoes[sessao] > sessao_obj.vagas:
+            raise forms.ValidationError(
+                _(f"O número de inscritos na sessão da atividade \"{sessao_obj.atividadeid.nome}\", das {sessao_obj.horarioid.inicio.strftime('%H:%M')} às {sessao_obj.horarioid.fim.strftime('%H:%M')} ({sessoes[sessao]} inscritos) excede o nº de vagas para essa sessão ({sessao_obj.vagas} vagas). Este erro pode ter ocorrido porque foi submetida entretanto uma outra inscrição que ocupou as vagas pretendidas. Por favor, atualize as suas inscrições nas sessões."))
+        if sessoes[sessao] > nalunos:
+            raise forms.ValidationError(
+                _(f"O número de inscritos na sessão da atividade \"{sessao_obj.atividadeid.nome}\", das {sessao_obj.horarioid.inicio.strftime('%H:%M')} às {sessao_obj.horarioid.fim.strftime('%H:%M')} ({sessoes[sessao]} inscritos) excede o nº de alunos disponíveis nesse horário ({nalunos} alunos)."))
+        sessoes_simultaneas = []
+        for inscritos_horario in inscritos_horarios:
+            if inscritos_horario['sessao'] is not sessao and horarios_intersetam(sessao_obj.horarioid.inicio, sessao_obj.horarioid.fim, inscritos_horario['inicio'], inscritos_horario['fim']):
+                nalunos_horario -= inscritos_horario['ninscritos']
+                sessoes_simultaneas.append(inscritos_horario)
+        if sessoes[sessao] > nalunos_horario:
+            quote = '"'
+            raise forms.ValidationError(
+                _(f"O número de inscritos na sessão da atividade \"{sessao_obj.atividadeid.nome}\", das {sessao_obj.horarioid.inicio.strftime('%H:%M')} às {sessao_obj.horarioid.fim.strftime('%H:%M')} ({sessoes[sessao]} inscritos) excede o nº de alunos disponíveis nesse horário ({nalunos_horario} alunos). Sessões simultâneas: {', '.join([quote + Sessao.objects.get(pk=sessao_simultanea['sessao']).atividadeid.nome + quote + ' das ' + sessao_simultanea['inicio'].strftime('%H:%M') + ' às ' + sessao_simultanea['fim'].strftime('%H:%M') + ' (' + str(sessao_simultanea['ninscritos']) + ' inscritos)' for sessao_simultanea in sessoes_simultaneas])}."))
 
 
 class SessoesForm(forms.Form):
@@ -81,15 +93,12 @@ class SessoesForm(forms.Form):
             pattern = re.compile('{(\"\d*\":\d*,)*\"\d*\":\d*}|{}')
             if re.match(pattern, cleaned_data['sessoes']) is None:
                 raise Exception()
-            cleaned_data['sessoes'] = json.loads(cleaned_data['sessoes'])
+            _sessoes = json.loads(cleaned_data['sessoes'])
+            cleaned_data['sessoes'] = {sessao: _sessoes[sessao] for sessao in _sessoes if _sessoes[sessao] > 0}
         except:
             raise forms.ValidationError(
                 _("Ocorreu um erro inesperado. Por favor, tente submeter uma nova inscrição."))
-        vazio = True
-        for sessaoid in cleaned_data['sessoes']:
-            if cleaned_data['sessoes'][sessaoid] > 0:
-                vazio = False
-        if not cleaned_data['sessoes'] or vazio:
+        if not cleaned_data['sessoes']:
             raise forms.ValidationError(
                 _("Deve inscrever-se, no mínimo, em uma sessão."))
         verificar_vagas(cleaned_data['sessoes'], cleaned_data['nalunos'])
