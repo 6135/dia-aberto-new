55c282524aa21d4e3a966545c3b09f8ffdea0a3e
André Barrocas <andrebarrocas99@hotmail.com>
2020-06-21 19:45:14 +0100

    Apagar notificações e melhorias

--- colaboradores/views.py
+++ colaboradores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from utilizadores.models import *
 from configuracao.models import *
