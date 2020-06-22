e755a953a50e9d59810f851af25b4efc6662d08f
André Barrocas <andrebarrocas99@hotmail.com>
2020-06-22 21:14:38 +0100

    links das mensagens

--- notificacoes/urls.py
+++ notificacoes/urls.py
@@ -6,9 +6,7 @@ from django.urls import re_path as pattern
 app_name = 'notificacoes'
 
 urlpatterns = [
-    path('enviar', views.EnviarNotificacao, name="enviar-notificacao"),
-    path('<int:pk>/', views.DetalhesNotificacao,
-         name="detalhes-notificacao"),
+
 
     path('detalhes/<int:id>', views.sem_notificacoes,
          name='sem-notificacoes'),
@@ -21,6 +19,27 @@ urlpatterns = [
          name='limpar-notificacoes'),
     path('marcarcomolida', views.marcar_como_lida,
          name='ler-notificacoes'),
+
     #     path('notificar_teste/<str:sigla>/<int:id>', views.enviar_notificacao_automatica,
     #          name='notificar'),
+
+    ################################ Mensagens ###########################
+
+    path('escolhertipo', views.escolher_tipo, name="enviar-notificacao"),
+    path('criarmensagem/<int:id>', views.criar_mensagem, name="escrever-mensagem"),
+    path('criarmensagemuo/<int:id>', views.criar_mensagem_uo, name="criar-mensagem-uo"),
+    path('criarmensagemadmin/<int:id>', views.criar_mensagem_admin, name="criar-mensagem-admin"),
+    path('criarmensagemparticipante/<int:id>', views.criar_mensagem_participante, name="criar-mensagem-participante"),
+    path('mensagens/<int:id>', views.sem_mensagens,
+         name='sem-mensagens'),
+    path('concluirenvio', views.concluir_envio,
+         name='concluir-envio'),
+    path('mensagens/<int:id>/<int:nr>', views.detalhes_mensagens,
+         name='detalhes-mensagem'),
+    path('apagarmensagem/<int:id>/<int:nr>', views.apagar_mensagem,
+         name='apagar-mensagem'),
+    path('limparmensganes/<int:id>', views.limpar_mensagens,
+         name='limpar-mensagens'),
+    path('marcarmensagemcomolida', views.mensagem_como_lida,
+         name='ler-mensagens'),
 ]
