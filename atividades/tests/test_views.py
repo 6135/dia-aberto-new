ce7118da6765ae9bcdc1142683e0bd9a3f343b7e
Manilha7 <a58604@ualg.pt>
2020-06-29 04:33:10 +0100

    up testes views

--- atividades/tests/test_views.py
+++ atividades/tests/test_views.py
@@ -8,7 +8,7 @@ from coordenadores.models import *
 from notificacoes.models import *
 from atividades.models import *
 from inscricoes.models import *
-from configuracao.tests.test_models import create_campus, create_edificio, create_horario, create_open_day, create_sala
+from configuracao.tests.test_models import create_campus, create_edificio, create_horario, create_open_day, create_sala, create_transporteH
 from utilizadores.tests.test_models import create_Administrador_0, create_Colaborador_0, create_Coordenador_0, create_Coordenador_1, create_Participante_0, create_ProfessorUniversitario_0, create_ProfessorUniversitario_1, create_Utilizador_0
 import json
 import urllib
@@ -464,3 +464,37 @@ class TestconfirmarResumoView(TestCase):
         self.assertEquals(response.context['tipo'], 'error')
         self.assertEquals(
             response.context['m'], 'Não tem permissões para esta ação!')
+
+
+
+class TestAtribuicaoTransporteView(TestCase):
+    
+
+    @classmethod
+    def setUp(self):
+        self.admin = create_Administrador_0()
+        self.diaaberto= create_open_day()
+        self.transporteH = create_transporteH(self.diaaberto)
+
+    def test_AtribuicaoTransporte_GET_semLogin(self):
+        response = self.client.get(
+            reverse('configuracao:atribuirTransporte', kwargs={'id': self.transporteH.id}))
+        self.assertRedirects(response, reverse('utilizadores:login'))
+
+    
+    def test_AtribuicaoTransporte_GET_naoAdmin(self):
+        utilizadores = [create_Utilizador_0(),
+                create_Colaborador_0(),
+                create_Participante_0(),
+                create_Coordenador_0(),
+                create_Participante_0(),]
+        for utilizador in utilizadores:
+            self.client.force_login(utilizador)
+            response = self.client.get(
+                reverse('configuracao:atribuirTransporte', kwargs={'id': self.transporteH.id}))
+            self.assertTemplateUsed(response, 'mensagem.html')
+            self.assertEquals(response.context['tipo'], 'error')
+            self.assertEquals(
+                response.context['m'], 'Não tem permissões para aceder a esta página!')
+            self.client.logout()
+#
