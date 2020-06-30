4e2203f27d2b7a5c216ab1b611c3046225b3b918
André Barrocas <andrebarrocas99@hotmail.com>
2020-06-30 18:57:29 +0100

    Adicionadas tabelas colaboradores Disponibilidade

--- colaboradores/models.py
+++ colaboradores/models.py
@@ -1,3 +1,34 @@
 
 from django.db import models
+from datetime import datetime, date,timezone,time
 
+class Disponibilidade(models.Model):
+    colab = models.ForeignKey('utilizadores.Colaborador', models.CASCADE, db_column='ColaboradorUtilizadorID',null=True,blank=True) 
+    class Meta:
+        db_table = 'Disponibilidade'
+
+
+class ColaboradorHorario(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    disponibilidade = models.ForeignKey(Disponibilidade, models.CASCADE, db_column='Disponibilidade',null=True,blank=True) 
+    inicio = models.TimeField(db_column='Inicio') 
+    fim = models.TimeField(db_column='Fim') 
+
+    class Meta:
+        db_table = 'ColaboradorHorario'
+
+
+class Preferencia(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    disponibilidade = models.ForeignKey(Disponibilidade, models.CASCADE, db_column='Disponibilidade',null=True,blank=True) 
+    tipoTarefa = models.CharField(db_column='Tipo', max_length=64) 
+    class Meta:
+        db_table = 'Preferencia'
+
+
+class PreferenciaAtividade(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True) 
+    atividade = models.ForeignKey('atividades.Atividade', models.CASCADE, db_column='Atividade',null=True,blank=True) 
+
+    class Meta:
+        db_table = 'PreferenciaAtividade'        
