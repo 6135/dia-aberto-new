66e1b2580ae95e03adc7e1f331612f7b77888ada
André Barrocas <andrebarrocas99@hotmail.com>
2020-07-01 11:16:24 +0100

    Pagina de disponibilidade do colaborador

--- colaboradores/views.py
+++ colaboradores/views.py
@@ -8,8 +8,8 @@ from colaboradores.models import *
 from notificacoes.models import *
 from django.shortcuts import redirect
 from .forms import *
-from .tables import TarefasTable
-from .filters import TarefasFilter
+from .tables import *
+from .filters import *
 from django.contrib.auth import *
 from django.core.mail import send_mail
 from django.conf import settings
@@ -23,6 +23,8 @@ from notificacoes import views
 from django.utils.datetime_safe import date
 from utilizadores.views import user_check
 from django.forms.models import modelformset_factory
+
+
 
 class consultar_tarefas(SingleTableMixin, FilterView):
 	''' Funcionalidade de consultar tarefas do colaborador atual, funcionalidades de filtros para a a consulta das tarefas '''
@@ -43,6 +45,67 @@ class consultar_tarefas(SingleTableMixin, FilterView):
 
 
 
+class AtividadesColaborador(SingleTableMixin, FilterView):
+	''' Página onde o colaborador escolhe quais são as atividades pelo qual está interessado'''
+	table_class = ColaboradorAtividadesTable
+	template_name = 'colaboradores/preferencia_atividade.html'
+	filterset_class = ColaboradorAtividadeFilter
+	table_pagination = {
+		'per_page': 10
+	}
+	
+	def dispatch(self, request, *args, **kwargs):
+		user_check_var = user_check(request=request, user_profile=[Colaborador])
+		if not user_check_var.get('exists'): return user_check_var.get('render')
+		self.user_check_var = user_check_var
+		return super().dispatch(request, *args, **kwargs)
+
+	def get_queryset(self):
+		return self.user_check_var.get('firstProfile').get_preferencia_atividade()
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		table = self.get_table(**self.get_table_kwargs())
+		context["deps"] = list(map(lambda x: (x.id, x.nome), Departamento.objects.filter(unidadeorganicaid=self.user_check_var.get('firstProfile').faculdade)))
+		context[self.get_context_table_name(table)] = table
+		return context
+
+
+
+def ver_departamentos(request):
+	user_check_var = user_check(request=request, user_profile=[Colaborador])
+	if not user_check_var.get('exists'): return user_check_var.get('render')
+	self.user_check_var = user_check_var
+
+	value_uo = request.POST.get("value_uo")
+	value_dep = request.POST.get('value_dep')
+
+	value_uo = is_int(value_uo)
+	if value_uo != 'None' and value_uo is not None and value_uo is not False:
+		uo= Unidadeorganica.objects.filter(id=value_uo).first()
+		departamentos= Departamento.objects.filter(unidadeorganicaid=  uo)
+	else:
+		departamentos= Departamento.objects.filter(unidadeorganicaid=self.user_check_var.get('firstProfile').faculdade)
+
+	deps= []
+	for dep in departamentos:    
+		deps.append({'key': dep.id ,'value': dep.nome})
+	value_dep = is_int(value_dep)
+	default= {}
+	if value_dep != 'None' and value_dep is not None and value_dep is not False:
+		dep= Departamento.objects.get(id=value_dep)
+		default={
+					'key': dep.id,
+					'value': dep.nome
+		} 
+	else:
+		default={
+						'key': "",
+						'value': "Qualquer Departamento"
+			} 
+	return render(request=request,template_name='configuracao/dropdown.html',context={'options':deps, 'default': default})   
+
+
 def minha_disponibilidade(request): 
 	''' Página onde o colaborador seleciona a disponibilidade para desempenhar tarefas escolhendo desta forma o(s) horário(s) que lhe dá mais jeito '''
 	if request.user.is_authenticated:    
@@ -56,11 +119,13 @@ def minha_disponibilidade(request):
 
 	HorarioFormSet = preferenciaHorarioFormset()
 	horario_form_set = HorarioFormSet(queryset=ColaboradorHorario.objects.none())
+	form_preferencia_tarefas = PreferenciaTarefasForm()	
+	msg = False
 
 	if request.method == "POST":
 		horario_form_set = HorarioFormSet(request.POST)
 		values = PreferenciaTarefasForm(request.POST)	
-		if form_transport.is_valid() and form_universitario.is_valid() and horario_form_set.is_valid():
+		if values.is_valid() and horario_form_set.is_valid():
 
 			instances = horario_form_set.save(commit=False)
 
@@ -78,12 +143,13 @@ def minha_disponibilidade(request):
 				Preferencia(colab = u.id, tipoTarefa = "tarefaOutra") 
 
 			return redirect('colaboradores:concluir-disponibilidade')
-	else:
-		form_preferencia_tarefas = PreferenciaTarefasForm()	
+		else:
+			msg = True
+			
 	return render(request = request,
 				template_name='colaboradores/minha_disponibilidade.html',
 				context={'form_preferencia_tarefas': form_preferencia_tarefas,
-				'horario_form_set': horario_form_set,})
+				'horario_form_set': horario_form_set,'dias' : Diaaberto.current().days_as_array() , 'msg' : msg,})
 
 
 def preferenciaHorarioFormset(extra = 0, minVal = 1):
