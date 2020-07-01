8be4484432266138462e2cdeb7c0a3380d3a0221
André Barrocas <andrebarrocas99@hotmail.com>
2020-07-01 01:25:28 +0100

    Minha disponibilidade

--- utilizadores/views.py
+++ utilizadores/views.py
@@ -905,7 +905,10 @@ def mensagem(request, id, *args, **kwargs):
         tipo = "info"  
     elif id == 16:
         m = "Para puder apagar a sua conta deverá concluir primeiro as tarefas que estão iniciadas"
-        tipo = "info"                
+        tipo = "info"     
+    elif id == 17:
+        m = "A sua disponibilidade foi alterada com sucesso"
+        tipo = "success"                   
     else:
         m = "Esta pagina não existe"
         tipo = "error"                                     
