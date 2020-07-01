9ea69a22c1df69bed7ac21c781af80dcb30bb8f6
André Barrocas <andrebarrocas99@hotmail.com>
2020-07-01 01:17:18 +0100

    Modelos colaboradores corrigidos

--- colaboradores/models.py
+++ colaboradores/models.py
@@ -2,15 +2,12 @@
 from django.db import models
 from datetime import datetime, date,timezone,time
 
-class Disponibilidade(models.Model):
-    colab = models.ForeignKey('utilizadores.Colaborador', models.CASCADE, db_column='ColaboradorUtilizadorID',null=True,blank=True) 
-    class Meta:
-        db_table = 'Disponibilidade'
 
 
 class ColaboradorHorario(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True) 
-    disponibilidade = models.ForeignKey(Disponibilidade, models.CASCADE, db_column='Disponibilidade',null=True,blank=True) 
+    colab = models.ForeignKey('utilizadores.Colaborador', models.CASCADE, db_column='ColaboradorUtilizadorID',null=True,blank=True) 
+    dia = models.DateField()
     inicio = models.TimeField(db_column='Inicio') 
     fim = models.TimeField(db_column='Fim') 
 
@@ -20,7 +17,7 @@ class ColaboradorHorario(models.Model):
 
 class Preferencia(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True) 
-    disponibilidade = models.ForeignKey(Disponibilidade, models.CASCADE, db_column='Disponibilidade',null=True,blank=True) 
+    colab = models.ForeignKey('utilizadores.Colaborador', models.CASCADE, db_column='ColaboradorUtilizadorID',null=True,blank=True) 
     tipoTarefa = models.CharField(db_column='Tipo', max_length=64) 
     class Meta:
         db_table = 'Preferencia'
@@ -28,6 +25,7 @@ class Preferencia(models.Model):
 
 class PreferenciaAtividade(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True) 
+    preferencia = models.ForeignKey(Preferencia, models.CASCADE, db_column='PreferenciaID',null=True,blank=True) 
     atividade = models.ForeignKey('atividades.Atividade', models.CASCADE, db_column='Atividade',null=True,blank=True) 
 
     class Meta:
